31cfdcb61b6919b8b9e2e3a0dea29c8b22d377b6
Jesus Canales <jesuscanales@outlook.com>
2023-06-06 16:03:53 -0500

    Hemos creado al Hola Mundo

--- class05/Main.java
+++ class05/Main.java
@@ -3,5 +3,6 @@ package class05;
 public class Main {
     public static void main(String [] args){
         System.out.println("Hola Mundo");
+        System.out.println("probamos el ide");
     }
 }
